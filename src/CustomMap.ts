a43a3933eb043258623b64ae25af88d5054e5614
yasirghouri <yasir.ag7@gmail.com>
2022-06-05 13:11:14 +0500

    added marker on map

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,10 +1,30 @@
+import { Company } from "./Company";
+import { User } from "./User";
+
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
-  constructor() {
-    this.googleMap = new google.maps.Map(document.getElementById("map"), {
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
       zoom: 1,
       center: { lat: 0, lng: 0 },
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable?.location?.lat,
+        lng: mappable?.location?.lng,
+      },
     });
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+import { Company } from "./Company";
 import { CustomMap } from "./CustomMap";
+import { User } from "./User";
 
-new CustomMap();
+const user = new User();
+const company = new Company();
+const customMap = new CustomMap("map");
+
+customMap.addMarker(user);
+customMap.addMarker(company);
