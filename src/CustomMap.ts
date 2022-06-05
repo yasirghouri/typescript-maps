67b17847c578a0afce8ae44a5aae32a3b70d8cbf
yasirghouri <yasir.ag7@gmail.com>
2022-06-05 12:59:28 +0500

    integrating google map

---
+++ src/CustomMap.ts
@@ -0,0 +1,10 @@
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor() {
+    this.googleMap = new google.maps.Map(document.getElementById("map"), {
+      zoom: 1,
+      center: { lat: 0, lng: 0 },
+    });
+  }
+}
